c50087ee4b5d7cc369f53c232623a6f0a1e34141
Sebastien Renault <sre@zenexity.com>
2014-12-01 09:39:37 +0100

    Add scripts to update source code from cheminot.m & cheminot.c

---
+++ cheminotc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash$
+
+cp ../cheminot.c/cheminotc.cpp src/android/jni/cheminotc/
+cp ../cheminot.c/cheminotc.h src/android/jni/cheminotc/
---
+++ cheminotm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash$
+
+rm -r src/android/jni
+cp -r ../cheminot.m/app/platforms/android/jni src/android/
