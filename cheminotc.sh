ab20389391bc229fbe6c1e698905f79d781420ab
Sebastien Renault <srenault.contact@gmail.com>
2015-04-07 19:34:10 +0200

    Commit config.h

--- cheminotc.sh
+++ cheminotc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash$
 
+cp  ~/data/bin/protobuf/config.h src/android/jni/protobuf/
 rsync -av ~/data/bin/protobuf/src/google/ src/android/jni/protobuf/google/
 rsync -av ../cheminot.c/src/protobuf/ src/android/jni/cheminotc/protobuf/
 rsync -av ../cheminot.c/src/fastmktime/ src/android/jni/cheminotc/fastmktime/
